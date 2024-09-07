c251e6a954aa12ba7afefcde32e2ac311355fe5b
Somen02 <somen30biswal@gmail.com>
2024-09-07 23:30:36 +0530

    Sample project commit_1

---
+++ example-service/example-service/src/main/java/com/practice/example_service/ExampleServiceApplication.java
@@ -0,0 +1,15 @@
+package com.practice.example_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ExampleServiceApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext run = SpringApplication.run(ExampleServiceApplication.class, args);
+	}
+
+}
